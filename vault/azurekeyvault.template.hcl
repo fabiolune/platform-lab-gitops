3ff6e9cef8cb7c9ac74633fc9baffa5a8144b92a
fabio.lonegro <fabio.lonegro@deltatre.com>
2021-06-16 17:22:11 +0200

    added vault with sealed secret for azure KV

--- vault/azurekeyvault.template.hcl
+++
@@ -1,8 +0,0 @@
-seal "azurekeyvault" {
-          tenant_id       = "__tenant_id__"
-          client_id       = "__client_id__"
-          client_secret   = "__client_secret__"
-          vault_name      = "__vault_name__"
-          key_name        = "__key_name__"
-          subscription_id = "__subscription_id__"
-        }
--- vault/local-kind/generate_sealed_secret.sh
+++ vault/local-kind/generate_sealed_secret.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-kubectl create secret generic azure-keyvault \
-    --from-literal=azurekeyvault.hcl="`cat ../azurekeyvault.template.hcl | sed s/__tenant_id__/$(cat .tenant_id)/i | sed s/__client_id__/$(cat .client_id)/i | sed s/__client_secret__/$(cat .client_secret)/i | sed s/__subscription_id__/$(cat .subscription_id)/i | sed s/__vault_name__/$(cat .vault_name)/i | sed s/__key_name__/$(cat .key_name)/i `" \
-    --dry-run=client -o yaml | \
-    kubeseal --format=yaml --cert=../../clusters/local-kind/pub-sealed-secrets.pem > secrets/azurekeyvault-sealed.yaml
+kubectl create secret generic azure-keyvault --from-file=../azurekeyvault.hcl --dry-run=client -o yaml > temp.yaml
+kubeseal --format=yaml --cert=../../clusters/local-kind/pub-sealed-secrets.pem < temp.yaml > secrets/azurekeyvault-sealed.yaml
+rm -f temp.yaml
